d63a3daf5e90c0d49622d3de833e5bf7a32df655
heikhatech <heikhafid@gmail.com>
2024-12-13 01:34:06 +0700

    Update LoadUnload.jsx

--- components/form/LoadUnload.jsx
+++ components/form/LoadUnload.jsx
@@ -50,7 +50,7 @@ const LoadUnload = () => {
           onClick={(event) =>
             handleDownload(
               resumeData,
-              resumeData.name + " by ATSResume.json",
+              resumeData.name + " by cvatsku.json",
               event
             )
           }
